0e4eea0ff9d593c28f13349bd36261a896fc4bf2
zapagenrevdale <zapagenrevdale@gmail.com>
2023-07-05 19:46:02 +0800

    update read me

--- public/views/index.view.php
+++ public/views/index.view.php
@@ -7,7 +7,7 @@
 
 <div class="flex flex-col justify-center items-center">
     <?php view("components/home/hero.php") ?>
-    <?php view("components/home/categories.php", ["products" => $products]) ?>
+    <?php view("components/home/products.php", ["products" => $products]) ?>
     <?php view("components/home/tutorials.php") ?>
 </div>
 

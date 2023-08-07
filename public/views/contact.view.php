7c66e4d24db3e734ef9f1fb5659a7cc3d9a663d0
zapagenrevdale <zapagenrevdale@gmail.com>
2023-08-07 19:18:15 +0800

    Update contacts

--- public/views/contact.view.php
+++ public/views/contact.view.php
@@ -26,7 +26,7 @@
                         d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                 </svg>
                 <a href="mailto:thegymbuilder014@gmail.com" class="hover:underline underline-offset-4">
-                    thegymbuilder014@gmail.com
+                    ravenaraza62@gmail.com
                 </a>
             </div>
 
@@ -37,10 +37,7 @@
                         d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                 </svg>
                 <div class="flex items-center gap-2">
-                    <a href="tel:+639350398535" class="hover:underline underline-offset-4"> 09350398535 </a>
-                    /
-                    <a href="tel:+639499356516" class="hover:underline underline-offset-4"> 09499356516 </a>
-
+                    <a href="tel:+639350398535" class="hover:underline underline-offset-4"> 09674118397 </a>
                 </div>
             </div>
 
